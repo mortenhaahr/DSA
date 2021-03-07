b0ab956e108ed6d06545984d78c4eac00b828be3
Mephisteles <peerweidemann21@hotmail.de>
2021-03-07 23:15:43 +0100

    Added two new empty functions to be used for the Decoding

---
+++ Case 2/FSKDecoder.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = FSKDecoder(x, fstart, fstop, symbolDuration, fsample)
+%FSKDECODER Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = fstart;
+outputArg2 = fstop;
+end
+
