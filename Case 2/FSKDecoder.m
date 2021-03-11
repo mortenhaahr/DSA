34c56a8f491d45f3d34ffc893251edcad5da83ba
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2021-03-11 14:55:10 +0100

    Added zero padding, changed where window is applied, and changed specifiedBinDFT. Now it doesnt transpose x on every iteration and sums in the for-loop

--- Case 2/FSKDecoder.m
+++ Case 2/FSKDecoder.m
@@ -1,26 +1,36 @@
 function [string] = FSKDecoder(x, fstart, fstop, fsample, symbolDuration)
-    
     N_full = length(x); %Determine number of samples
     DFTBlockSize = fsample*symbolDuration; %Calculate size of DFT-block according to symbolDuration
-    bin_res = fsample/DFTBlockSize;
     numOfChars = floor(N_full/DFTBlockSize); %Calculate number of chars in signal
-    freqArray = linspace(fstart, fstop, 256); %Restore original frequency array to determine bins to calculate
-    binIndices = round(freqArray/bin_res); %Indices of bins to calculate
-    wGauss = gausswin(length(x)); 
-    x = x.*wGauss; %Adding Gauss window function to reduce spectral leakage
     
     string = zeros(1, numOfChars);
     k = 1;
     for i = 1:DFTBlockSize:numOfChars*DFTBlockSize % For loop iterating over number of chars with changing block start and stop samples
-        x_block = x(i:i+DFTBlockSize-1);
         
+        % Defining block and applying window:
+        x_block = x(i:i+DFTBlockSize-1);
+        wGauss = gausswin(length(x_block)); 
+        x_block = x_block.*wGauss; %Adding Gauss window function to reduce spectral leakage
+        
+        % Applying zero padding if needed:
+        if(length(x_block) < fsample/2) % If length < fs/2, apply zero padding
+            x_block = [x_block; zeros(fsample/2-length(x_block), 1)];
+        end
+        
+        % Finding bin_res and necessary bins to calculate for:
+        bin_res = fsample/length(x_block);
+        freqArray = linspace(fstart, fstop, 256*bin_res); %Restore original frequency array to determine bins to calculate
+        binIndices = round(freqArray/bin_res); %Indices of bins to calculate
+        
+        % Calculating frequency bins:
+        X = zeros(1, length(binIndices));
         for j = 1:length(binIndices) % For loop iterating over all possible frequency bins
             X(j) = specifiedBinDFT(x_block, binIndices(j));
         end
 
         [~, highest_bin] = max(abs(X)); %Determining the bin index of highest peak which is the frequency of the char
  
-        string(k) = highest_bin;
+        string(k) = highest_bin/bin_res; %Value of the char. 
         k = k+1;
     end
     string = char(string); % Convert indices to chars
--- Case 2/case2.m
+++ Case 2/case2.m
@@ -62,64 +62,22 @@ ylim([fstart/1000 fstop/1000]);
 clf; clear; clc; close all;
 [x, fsample] = audioread('lydsignal_0.wav');
 %soundsc(x, fsample);
-x = x';
-
+x = x(:,1);
 
 % Test to see if X_ours calculates the same as X_matlab
 X_matlab = fft(x);
-%X_ours = specifiedBinDFT(x,1, 50);
 
-% delta = 0.1;
-% for i = 1:length(X_ours)
-%     if(abs(X_ours) + delta < abs(X_matlab(i)))
-%         display(["X_ours: ", num2str(abs(X_ours)), " X_matlab: ", (abs(X_matlab))]);
-%     end
-% end
-
-% X_ours = zeros(1, 100);
-% for i = 1:100
-%     X_ours(i) = specifiedBinDFT(x,i);
-% end
-
-for i = 1:length(X_ours)
-    display([num2str(X_ours(i)), num2str(X_matlab(i))]);
+X_ours = zeros(1, 251);
+for i = 1:250
+    X_ours = specifiedBinDFT(x, i);
+    display(num2str(X_ours) + " " + num2str(X_matlab(i)));
 end
 
-% Split X into blocks for every symbol
-X = fft(x);
-symbolDuration = 0.5; %Seconds
-blockSamples = fsample*symbolDuration;
-N_blocks = floor(length(X)/blockSamples);
-X_block = zeros(N_blocks, blockSamples);
-
-% for i = 1 : ceil(length(X)/blockSamples)
-%     if(i ~= N_blocks-1)
-%         if(i == 1)
-%             X_block(i,:) = fft(x(i+(i-1)*blockSamples:i*blockSamples));
-%         else
-%             fft(x(i*blockSamples:i
-%         end
-%     end
-% end
-
-% STFT Manual:
-N = blockSamples;
-f_axis = 0:fsample/N:fsample-fsample/N;
-for i = 1 : blockSamples : N_blocks*blockSamples
-    n_block = ceil(i/blockSamples); % Ceil because of 1-indexing
-    X_block(n_block,:) = fft(x(i:blockSamples+i-1));
-    figure();
-    stem(f_axis ,abs(X_block(n_block,:)));
-    str = "DFT of FSK signal fra sample " + num2str(i) + " - " + num2str(blockSamples+i-1);
-    title(str);
-    xlabel('Frequency [Hz]');
-    ylabel('Magnitude');
-    xlim([1000 1500]);
-end
 
 %% Opgave 2:
 clf; clear; clc; close all;
 [x, fsample] = audioread('lydsignal_0.30sec.wav');
+%x = x(:,1);
 
 % STFT Manual:
 fstart = 1000; %Hz
