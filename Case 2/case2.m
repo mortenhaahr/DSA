98ab797e5f5c71c83ff248689df3e4d7ee1efb57
Mephisteles <peerweidemann21@hotmail.de>
2021-03-12 21:10:11 +0100

    Minor changes

--- Case 2/case2.m
+++ Case 2/case2.m
@@ -88,25 +88,24 @@ decodedString = FSKDecoder(x, fstart, fstop, fsample, symbolDuration);
 
 %% Opgave 3:
 clf; clear; clc; close all;
-[x, fsample] = audioread('lydsignal_0.wav'); % Change this for other values.
+[x, fsample] = audioread('lydsignal_250cm.wav'); % Change this for other values. Measurements taken with 0.5 sec symbolDuration
 % Note: Some distances are not nice because of noise in the start.
+symbolDuration = 0.5;
 
-x = x(1:24000); % Only looking at the first symbol
+x = x(1:symbolDuration*fsample); % Only looking at the first symbol
+
+SNR_ours = mySNR(x);
+SNR_matlab = snr(x);
 
 N = length(x);
 Xabs = abs(fft(x));
 X_powerspectrum = (Xabs.*Xabs)/(N*N);
 X_powerdb = 10*log10(X_powerspectrum);
 
-
-SNR_ours = mySNR(x);
-SNR_matlab = snr(x);
-
-
 f_axis = 0:fsample/N:fsample-fsample/N;
 figure();
 plot(f_axis,X_powerdb,'*');
-title('DFT of FSK signal');
+title('Powerspectrum of FSK signal 250cm');
 xlabel('Frequency [Hz]');
 ylabel('Power [dB]');
 
@@ -119,12 +118,14 @@ clf; clear; clc; close all;
 [x200, fsample] = audioread('lydsignal_200cm.wav'); 
 [x250, fsample] = audioread('lydsignal_250cm.wav'); 
 
-x0 = x0(1:24000); % Only looking at the first symbol
-x50 = x50(1:24000); % Only looking at the first symbol
-x100 = x100(1:24000); % Only looking at the first symbol
-x150 = x150(1:24000); % Only looking at the first symbol
-x200 = x200(1:24000); % Only looking at the first symbol
-x250 = x250(1:24000); % Only looking at the first symbol
+symbolDuration = 0.5
+
+x0 = x0(1:symbolDuration*fsample); % Only looking at the first symbol
+x50 = x50(1:symbolDuration*fsample); % Only looking at the first symbol
+x100 = x100(1:symbolDuration*fsample); % Only looking at the first symbol
+x150 = x150(1:symbolDuration*fsample); % Only looking at the first symbol
+x200 = x200(1:symbolDuration*fsample); % Only looking at the first symbol
+x250 = x250(1:symbolDuration*fsample); % Only looking at the first symbol
 
 
 
@@ -148,11 +149,47 @@ ylabel('SNR [dB]');
 
 %% Opgave 4
 clf; clear; clc; close all;
-[x, fsample] = audioread('lydsignal_0.30sec.wav');
+[x, fsample] = audioread('lydsignal_0.20sec.wav');
 
 % STFT Manual:
 fstart = 1000; %Hz
 fstop = 2000; %Hz
-symbolDuration = 0.30;
+symbolDuration_0_50 = 0.50;
+symbolDuration_0_45 = 0.45;
+symbolDuration_0_40 = 0.40;
+symbolDuration_0_35 = 0.35;
+symbolDuration_0_30 = 0.30;
+symbolDuration_0_25 = 0.25;
+symbolDuration_0_20 = 0.20;
 
-decodedString = FSKDecoder(x, fstart, fstop, fsample, symbolDuration);
+%decodedString = FSKDecoder(x, fstart, fstop, fsample, symbolDuration);
+
+[x0_50s, fsample] = audioread('lydsignal_0.wav'); 
+[x0_45s, fsample] = audioread('lydsignal_0.45sec.wav'); 
+[x0_40s, fsample] = audioread('lydsignal_0.40sec.wav'); 
+[x0_35s, fsample] = audioread('lydsignal_0.35sec.wav'); 
+[x0_30s, fsample] = audioread('lydsignal_0.30sec.wav'); 
+[x0_25s, fsample] = audioread('lydsignal_0.25sec.wav'); 
+[x0_20s, fsample] = audioread('lydsignal_0.20sec.wav');
+
+x0_50s = x0_50s(1:symbolDuration_0_50*fsample); % Only looking at the first symbol
+x0_45s = x0_45s(1:symbolDuration_0_45*fsample); % Only looking at the first symbol
+x0_40s = x0_40s(1:symbolDuration_0_40*fsample); % Only looking at the first symbol
+x0_35s = x0_35s(1:symbolDuration_0_35*fsample); % Only looking at the first symbol
+x0_30s = x0_30s(1:symbolDuration_0_30*fsample); % Only looking at the first symbol
+x0_25s = x0_25s(1:symbolDuration_0_25*fsample); % Only looking at the first symbol
+x0_20s = x0_20s(1:symbolDuration_0_20*fsample); % Only looking at the first symbol
+
+snr0_50s = mySNR(x0_50s);
+snr0_45s = mySNR(x0_45s);
+snr0_40s = mySNR(x0_40s);
+snr0_35s = mySNR(x0_35s);
+snr0_30s = mySNR(x0_30s);
+snr0_25s= mySNR(x0_25s);
+snr0_20s= mySNR(x0_20s);
+
+n = fsample*symbolDuration_0_20;
+
+spectrogram(x, blackman(n), 0, n, fsample,'yaxis');
+ylim([fstart/1000 fstop/1000]);
+title('Spectrogram af signal med symbolDuration = 0.2sec');
