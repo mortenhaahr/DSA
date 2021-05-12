85e6f143de4d2be269286d99046aa0321519f167
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2021-05-12 09:30:43 +0200

    Now with correct sweep_sig_files

--- Case 4/case4.m
+++ Case 4/case4.m
@@ -6,7 +6,7 @@ set(0,'DefaultFigureWindowStyle','docked')
 fs = 48e03;
 fstart = 100;
 fstop = 3e03;
-sweep_length = 50;
+sweep_length = 500;
 t = 0:1/fs:sweep_length/fs-1/fs;
 t_last = t(sweep_length);
 sweep_sig = chirp(t, fstart, t_last, fstop);
@@ -17,4 +17,24 @@ title(['Chirp signal in time domain']);
 xlabel('Time [s]');
 ylabel('Amplitude');
 
-save('sweep_sig.mat', 'sweep_sig');
+
+% Eksempel på skrivning af array til fil, som kan læses ind i CrossCore
+powShort = 2^15; % Samme som shift med 15 bits
+fid=fopen('sweep_sig_500_s.dat', 'w');
+for i=1:length(sweep_sig)-1
+    fprintf(fid, '%d,\n', toRealnumber(sweep_sig(i), powShort)); 
+end
+fprintf(fid, '%d\n', toRealnumber(sweep_sig(i+1), powShort));   % Uden ","-tegn
+fclose(fid);
+
+% Funktion for convertering fra double/float til heltal
+function y = toRealnumber(x, multFactor)
+    y = round(x*multFactor);
+    if (y >= multFactor) 
+        y = multFactor-1; % Limitering af max værdi
+    end
+end
+
+
+
+
