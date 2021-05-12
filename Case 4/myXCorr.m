32303bdd3e08129db5934dee0037cad2f0012cf4
Mephisteles <peerweidemann21@hotmail.de>
2021-05-12 22:18:12 +0200

    Added xcorr function

--- Case 4/myXCorr.m
+++ Case 4/myXCorr.m
@@ -1,21 +1,20 @@
-function [crossCorrelation] = myXCorr(chirpSignal, receivedSignal)
+ function [R, lag] = myXCorr(chirpSignal, receivedSignal)
 %myXCorr Cross correlation function used to find out signal runtime to
 %object and back
-diff = abs(receivedSignal-chirpSignal);
-if(length(chirpSignal) > length(receivedSignal))
-    minLim = -length(chirpSignal);
-    maxLim = length(chirpSignal);
-    receivedSignal = receivedSignal(
-else
-    minLim = -length(receivedSignal);
-    maxLim = length(receivedSignal);
-end
-    
-N_xCorr = ((length(chirpSignal)+length(receivedSignal)-1);
 
-for
-    
-    
-    
-end
+N_received = length(receivedSignal);
+N_chirp = length(chirpSignal);
+N_xCorr = N_received+N_chirp;
 
+receivedSigPadded = [zeros(1, N_received), chirpSignal];
+R_offset = N_received;
+R = zeros(1, N_xCorr+N_received);
+lag = zeros(1, N_xCorr+N_received);
+for i = -N_received:N_xCorr-1
+    lag(i+N_received+1)= i;
+    for j = 1:N_received
+        if(N_received+j+i) <= N_xCorr
+            R(R_offset+1+i) = R(R_offset+1+i)+(receivedSignal(j)*receivedSigPadded(R_offset+j+i));
+        end
+    end 
+end
